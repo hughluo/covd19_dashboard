a9bd691862e74429fda5115a7e3d4be437d8a62d
Shamiani <shamiani@thinkport.digital>
2020-06-23 14:45:06 +0200

    changed graph

--- app.py
+++ app.py
@@ -2,29 +2,52 @@ import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
+import numpy as np
+import plotly.express as px
 
 import pandas as pd
 
 dashboard_name = "COVID-19 Dashboard"
-info = 'USA'
+info = 'Germany'
 csv_link = "https://covid19-lake.s3.us-east-2.amazonaws.com/enigma-aggregation/csv/global_countries/enigma_covid_19_global_countries.csv"
 df_raw = pd.read_csv(csv_link)
-df_usa = df_raw[df_raw['country_name'] == 'United States'][['date','cases', 'deaths','tests']].reset_index(drop=True)
+df_usa = df_raw[df_raw['country_name'] == 'United States'][['date', 'cases', 'deaths', 'tests']].reset_index(drop=True)
 timeline_info = "Timeline from https://www.nytimes.com/article/coronavirus-timeline.html"
-timeline = {
-    '2019-12-31': 'Chinese authorities treated dozens of cases of pneumonia of unknown cause.',
-    '2020-01-11': 'China reported its first death.',
-}
 
-timeline_options = [ {'label': v, 'value': k} for k, v in timeline.items()]
-df_usa['event'] = timeline[df_usa['date']]
 
+def increase_rate(df_raw, country, metric):
+    df = df_raw[df_raw['country_name'] == country][['country_name', 'date', metric]].reset_index(drop=True)
+    df['increase_rate'] = df.groupby('country_name')[[metric]].pct_change()
+    df.fillna(0, inplace=True)
+    df.replace([np.inf], 0, inplace=True)
+    res = df
+    return res
+
+
+# avg_increase_rate_before_event(event_time) = avg(increase_rate(origin to event))
+# avg_increase_rate_after_event(event_time) = avg(increase_rate(event to today))
+
+def avg_increase_rate(df_with_increase_rate, event_date, after=True):
+    df = df_with_increase_rate
+    df_event = df[df['date'] > event_date]
+    if not after:
+        df_event = df[df['date'] <= event_date]
+    res = df_event['increase_rate'].mean()
+    return res
+
+
+df_increase_rate = increase_rate(df_raw, 'Germany', 'cases')
+df_increase_rate
+
+avg_after_childrensday = avg_increase_rate(df_increase_rate, '2020-05-01')
+avg_before_childrensday = avg_increase_rate(df_increase_rate, '2020-05-01', False)
+print(avg_after_childrensday)
+print(avg_before_childrensday)
 
 
 def init(app):
-    app.css.append_css({'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'})
+  #  app.css.append_css({'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'})
 
-<<<<<<< HEAD
     metric_type_dropdown = dcc.Dropdown(
         id='metric_type',
         options=[
@@ -51,85 +74,35 @@ def init(app):
         value='Obligation to notify',
     )
 
-=======
-    timeline_dropdown = dcc.Dropdown(
-        id='timeline',
-        options=timeline_options,
-        value='event'
-    )
-
->>>>>>> usa
     app.layout = html.Div([
         html.H1(children=dashboard_name),
         html.H4(children=f'datasource: {csv_link}'),
         html.H6(children=info),
-<<<<<<< HEAD
         html.H2(children="Select metric type"),
         metric_type_dropdown,
         html.H2(children="Select country"),
         country_dropdown,
         html.H2(children="Select measure"),
         measure_dropdown,
-        dcc.Graph(id='my-graph')
-    ], style={'width': '500'})
 
-    @app.callback(Output('my-graph', 'figure'),
-                  [Input('metric_type', 'value'), Input('country', 'value'), Input('measure', 'value')])
-    def update_graph(metric_type, selected_country):
-        # selected_country = "Jordan"
-        df_date_world = df_raw[df_raw['country_name'] == 'World'].set_index(['date'])
-        df_out_world = df_date_world[metric_type].sort_index(axis=0).to_frame()
-        df_date_selected = df_raw[df_raw['country_name'] == selected_country].set_index(['date'])
-        df_out_selected = df_date_selected[metric_type].sort_index(axis=0).to_frame()
-        return {
-            'data': [
-                {
-                    'x': df_out_world.index,
-                    'y': df_out_world[metric_type],
-                    'name': "World",
-                    'marker': {'color': 'rgb(55, 83, 109)'},
-                },
-                {
-                    'x': df_out_selected.index,
-                    'y': df_out_selected[metric_type],
-                    'name': selected_country,
-                    'marker': {'color': 'rgb(26, 118, 255)'},
-                },
-=======
-        html.H3(children=timeline_info),
-        html.H2(children='selecet starting event'),
-        timeline_dropdown,
-        dcc.Graph(id='my-graph')
-    ], style={'width': '500'})
+        dcc.Graph(
+            id='my-graph',
+            config={'displayModeBar': False},
+            animate=True,
+            figure=px.line(df_raw,
+                           x='date',
+                           y='cases',
+                           color='country_name',
+                           template='plotly_dark').update_layout(
+                {'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+                 'paper_bgcolor': 'rgba(0, 0, 0, 0)'})
 
-    @app.callback(Output('my-graph', 'figure'), [Input('timeline', 'value')])
-    def update_graph(event):
-        # df_date_world = df_raw[df_raw['country_name'] == 'World'].set_index(['date'])
-        # df_out_world = df_date_world[metric_type].sort_index(axis = 0).to_frame()
-        # df_date_selected = df_raw[df_raw['country_name'] == selected_country].set_index(['date'])
-        # df_out_selected = df_date_selected[metric_type].sort_index(axis = 0).to_frame()
-        df_since_event = df_usa[df_usa['date'] >= event].set_index(['date'])
-        metric_type = 'cases'
-        fig = {
-            'data': [
-                {
-                'x': df_since_event.index,
-                'y': df_since_event[metric_type],
-                'name': f'{metric_type} stats for USA',
-                'marker': { 'color': 'rgb(55, 83, 109)' },
-                },
->>>>>>> usa
-            ],
-            'layout': {'margin': {'l': 40, 'r': 0, 't': 20, 'b': 30}}
-        }
-        return fig
+        )])
 
 
 if __name__ == '__main__':
-    app = dash.Dash(dashboard_name)
+    app = dash.Dash(dashboard_name, external_stylesheets=[
+        'https://codepen.io/chriddyp/pen/bWLwgP.css'
+    ])
     init(app)
-<<<<<<< HEAD
-    app.run_server(host='0.0.0.0', port=8050)
-=======
     app.run_server(host='0.0.0.0', port=8050, debug=True)
->>>>>>> usa
