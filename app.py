2e1f37474ed397081b8907946bfad62a37a007b4
Yinchi Luo <yluo@Yinchis-MBP.local>
2020-06-24 08:59:56 +0200

    fix graph

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ timeline_info = "Timeline from https://www.nytimes.com/article/coronavirus-timel
 
 
 def increase_rate(df_raw, country, metric):
-    df = df_raw[df_raw['country_name'] == country][['country_name', 'date', metric]].reset_index(drop=True)
+    df = df_raw[df_raw['country_name'] == country][[
+        'country_name', 'date', metric]].reset_index(drop=True)
     df['increase_rate'] = df.groupby('country_name')[[metric]].pct_change()
     df.fillna(0, inplace=True)
     df.replace([np.inf], 0, inplace=True)
@@ -37,71 +38,65 @@ def avg_increase_rate(df_with_increase_rate, event_date, after=True):
 
 
 df_increase_rate = increase_rate(df_raw, 'Germany', 'cases')
-df_increase_rate
 
 avg_after_childrensday = avg_increase_rate(df_increase_rate, '2020-05-01')
-avg_before_childrensday = avg_increase_rate(df_increase_rate, '2020-05-01', False)
-
-
+avg_before_childrensday = avg_increase_rate(
+    df_increase_rate, '2020-05-01', False)
 
 
 def init(app):
     #  app.css.append_css({'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'})
 
-    metric_type_dropdown = dcc.Dropdown(
-        id='metric_type',
-        options=[
-            {'label': 'Cases', 'value': 'cases'},
-            {'label': 'Deaths', 'value': 'deaths'},
-        ],
-        value='cases'
-    )
-
     country_dropdown = dcc.Dropdown(
         id='country',
-        options=[{'label': country, 'value': country} for country in df_raw['country_name'].unique()],
+        options=[{'label': country, 'value': country}
+                 for country in df_raw['country_name'].unique()],
         value='Germany'
     )
 
-    measure_dropdown = dcc.Dropdown(
-        id='measure',
-        options=[{'label': 'Obligation to notify', 'value': '2020-01-31'},
-                 {'label': 'Stop of entry', 'value': '2020-03-17'},
-                 {'label': 'Contact restrictions', 'value': '2020-03-22'},
-                 {'label': 'Quarantine obligation', 'value': '2020-04-10'},
-                 {'label': 'Masc obligation', 'value': '2020-04-22'},
-                 {'label': 'Strong relaxations of Corona measures', 'value': '2020-05-06'}, ],
+    event_dropdown = dcc.Dropdown(
+        id='event',
+        options=[
+            {'label': 'Obligation to notify', 'value': '2020-01-31'},
+            {'label': 'Stop of entry', 'value': '2020-03-17'},
+            {'label': 'Contact restrictions', 'value': '2020-03-22'},
+            {'label': 'Quarantine obligation', 'value': '2020-04-10'},
+            {'label': 'Masc obligation', 'value': '2020-04-22'},
+            {'label': 'Strong relaxations of Corona event', 'value': '2020-05-06'},
+        ],
         value='Obligation to notify',
     )
-
-    fig = px.line(df_raw,
-                  title="Effects of Corona Measures",
-                  x='date',
-                  y='cases',
-                  color='country_name',
-                  template='plotly_dark').update_layout(
-        {'plot_bgcolor': 'rgba(0, 0, 0, 0)',
-         'paper_bgcolor': 'rgba(0, 0, 0, 0)'})
-
-    fig.update_traces(mode="lines+markers")
 
     app.layout = html.Div([
         html.H1(children=dashboard_name),
         html.H4(children=f'datasource: {csv_link}'),
         html.H6(children=info),
-        html.H2(children="Select metric type"),
-        metric_type_dropdown,
         html.H2(children="Select country"),
         country_dropdown,
         html.H2(children="Select measure"),
-        measure_dropdown,
+        event_dropdown,
 
         dcc.Graph(
             id='my-graph',
             config={'displayModeBar': False},
             animate=True,
-            figure=fig
+            # figure=fig
         )])
+
+    @app.callback(Output('my-graph', 'figure'), [Input('event', 'value')])
+    def update_graph(event):
+        fig = go.Figure()
+        # Doc for go.Scatter: https://plotly.com/python/reference/#scatter
+        # Examples: https://plotly.com/python/line-charts/
+        fig.add_trace(go.Scatter(
+            x=df_increase_rate['date'],
+            y=df_increase_rate['increase_rate'], 
+            mode='lines+markers',
+            line=dict(color="black", width=2),
+            connectgaps=True,
+        ))
+
+        return fig
 
 
 if __name__ == '__main__':
